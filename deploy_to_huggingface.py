47f40dadf518596c0054d2b6bfa07097c9b9cb5e
Tayawelba Dawai Hesed <85948333+Tayawelba@users.noreply.github.com>
2024-05-30 01:08:39 +0100

    update file

--- deploy_to_huggingface.py
+++ deploy_to_huggingface.py
@@ -8,17 +8,15 @@ def deploy_to_huggingface():
 
     # Chemin du répertoire contenant les fichiers à télécharger (sauf upload_to_hf.py)
     folder_path = "."
-    # Liste des fichiers à exclure
-    exclude_files = ["upload_to_hf.py"]
 
     # Créer un objet HfFolder pour gérer le téléchargement des fichiers
-    hf_folder = HfFolder(folder_path)
+    hf_folder = HfFolder()
 
     # Upload de chaque fichier/dossier
-    for file_name in hf_folder.files():
-        if file_name not in exclude_files:
-            file_path = hf_folder.full_file_path(file_name)
-            hf_folder.push_to_hub(path=file_name, repo_id=space_name, token=hf_token)
+    for file_name in os.listdir(folder_path):
+        if file_name != "upload_to_hf.py":
+            file_path = os.path.join(folder_path, file_name)
+            hf_folder.push_to_hub(path=file_path, repo_id=space_name, token=hf_token)
 
 if __name__ == '__main__':
     deploy_to_huggingface()
